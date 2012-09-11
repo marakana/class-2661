e6ce34db5683245ff9c7b5e363e3b3f42a358509
Marko Gargenta <marko@marakana.com>
2012-09-11 02:28:46 -0700

    Added AsyncTask

--- Yamba/src/com/motorola/yamba/StatusActivity.java
+++ Yamba/src/com/motorola/yamba/StatusActivity.java
@@ -1,12 +1,14 @@
 package com.motorola.yamba;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.marakana.android.yamba.clientlib.YambaClient;
 
@@ -20,8 +22,8 @@ public class StatusActivity extends Activity implements OnClickListener {
         setContentView(R.layout.activity_status);
         
         // Workaround Motorola proxy
-        System.setProperty("http.proxyHost", "wwwgate0.mot.com");
-        System.setProperty("http.proxyPort", "1080");
+//        System.setProperty("http.proxyHost", "wwwgate0.mot.com");
+//        System.setProperty("http.proxyPort", "1080");
         
         editStatus = (EditText) findViewById(R.id.edit_status);
         buttonPost = (Button) findViewById(R.id.button_post);
@@ -32,10 +34,35 @@ public class StatusActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		String status = editStatus.getText().toString();
 		
-		YambaClient yambaClient = new YambaClient("student","password");
-		yambaClient.updateStatus(status);
+		new StatusUpdateTask().execute(status);
 		
 		Log.d("Yamba", "onClick'd status: "+status);
+	}
+	
+	class StatusUpdateTask extends AsyncTask<String, Void, String> {
+		YambaClient yambaClient;
+
+		@Override
+		protected void onPreExecute() {
+			yambaClient = new YambaClient("student","password");
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				yambaClient.updateStatus(params[0]);
+				return "Successfully posted";
+			} catch (Exception e) {
+				Log.e("Yamba", "Failed to post", e);
+				e.printStackTrace();
+				return "Failed to posted";
+			}				
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
 	}
 
 }
