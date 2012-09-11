a450af899234117dd88368e07187b60241ef5569
Marko Gargenta <marko@marakana.com>
2012-09-11 01:19:01 -0700

    Hooked up the UI

--- Yamba/src/com/motorola/yamba/StatusActivity.java
+++ Yamba/src/com/motorola/yamba/StatusActivity.java
@@ -1,20 +1,31 @@
 package com.motorola.yamba;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	Button buttonPost;
+	EditText editStatus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        
+        editStatus = (EditText) findViewById(R.id.edit_status);
+        buttonPost = (Button) findViewById(R.id.button_post);
+        buttonPost.setOnClickListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_status, menu);
-        return true;
-    }
+	@Override
+	public void onClick(View v) {
+		String status = editStatus.getText().toString();
+		Log.d("Yamba", "onClick'd status: "+status);
+	}
+
 }
