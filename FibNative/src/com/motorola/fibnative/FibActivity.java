a6f1fe720c0a96a147c7c9088a8d9c603ac8b350
Marko Gargenta <marko@marakana.com>
2012-09-12 00:42:07 -0700

    Implemented simple UI

--- FibNative/src/com/motorola/fibnative/FibActivity.java
+++ FibNative/src/com/motorola/fibnative/FibActivity.java
@@ -1,20 +1,31 @@
 package com.motorola.fibnative;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FibActivity extends Activity {
-
+	EditText input;
+	TextView output;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fib);
+        
+        input = (EditText) findViewById(R.id.input);
+        output = (TextView) findViewById(R.id.output);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_fib, menu);
-        return true;
+    public void onClickGo(View v) {
+    		long n = Long.parseLong( input.getText().toString() );
+    		
+    		long resultJ = FibLib.fibJ(n);
+    		output.append( String.format("\nfibJ(%d)=%d", n, resultJ));
+    		
+    		long resultN = FibLib.fibN(n);
+    		output.append( String.format("\nfibN(%d)=%d", n, resultN));
     }
 }
