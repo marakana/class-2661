246ca70030913cb3a6c5512c17a8d215bffdfeba
Marko Gargenta <marko@marakana.com>
2012-09-12 01:22:39 -0700

    Added Java iterative

--- FibNative/src/com/motorola/fibnative/FibLib.java
+++ FibNative/src/com/motorola/fibnative/FibLib.java
@@ -2,11 +2,24 @@ package com.motorola.fibnative;
 
 public class FibLib {
 
+	// Java Iterative
 	public static long fibJ(long n) {
 		if(n==0) return 0;
 		if(n==1) return 1;
 		return fibJ(n-1)+fibJ(n-2);
 	}
+	
+	// Java Native
+	public static long fibJI(long n) {
+        long previous = -1;
+        long result = 1;
+        for (long i = 0; i <= n; i++) {
+            long sum = result + previous;
+            previous = result;
+            result = sum;
+        }
+        return result;
+    }
 	
 	static {
 		System.loadLibrary("fib");
